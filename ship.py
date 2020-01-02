07c76c48a8f4f6f9ebabc66b6a0097ace6dae3db
高慧明 <gaohuiming@HMGao-MacBook-Pro.local>
2020-01-03 00:00:47 +0800

    add  ship speed

--- ship.py
+++ ship.py
@@ -7,9 +7,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """ 初始化飞船并设置其位置 """
         self.screen = screen
+        self.ai_settings = ai_settings
         
         # 加载图像并获取其外接距形
         self.image = pygame.image.load('images/ship.bmp')
@@ -19,6 +20,21 @@ class Ship():
         # 将每一艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom  = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        # move flag
+        self.move_right = False
+        self.move_left = False
+
+    def update(self):
+        """ 根据移动标志调整飞船的位置 """
+        if self.move_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.move_left:
+            self.center -= self.ai_settings.ship_speed_factor
+        
+        self.rect.centerx = self.center
 
     def blitme(self):
         """ 在指定位置绘制飞船 """
