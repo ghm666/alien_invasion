b81ee41ceed45c46740a526da17d0f141f993cad
ghm666 <1071527879@qq.com>
2020-01-13 22:56:12 +0800

    add bullet

--- settings.py
+++ settings.py
@@ -13,5 +13,11 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        
+
+        # ship 设置
         self.ship_speed_factor = 10
+        # 子弹设置
+        self.bullet_speed_factor = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
