3c8c2e59708c13f7bc8c64e60b188c520c9f684c
ghm666 <1071527879@qq.com>
2020-02-01 18:12:00 +0800

    add alien class

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from aliens import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -20,6 +21,9 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
+
 
     # 开始游戏的主循环
     while True:
@@ -27,7 +31,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
         
 
 if __name__ == "__main__":
--- game_functions.py
+++ game_functions.py
@@ -15,6 +15,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.move_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
+
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """ 如果还没到达限制，就发射一颗子弹 """
@@ -41,7 +44,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """ 更新屏幕上的图像，并切换到新屏幕 """
 
     # 每次循环时都重新绘制屏幕
@@ -51,6 +54,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+    alien.blitme()
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
